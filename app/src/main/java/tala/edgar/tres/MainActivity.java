eb665d83024f4e69aab244d6e047fea127729556
Edgar Rogelio <rogerestrad@gmail.com>
2018-05-07 13:26:27 -0500

    Initial commit

---
+++ app/src/main/java/tala/edgar/tres/MainActivity.java
@@ -0,0 +1,19 @@
+package tala.edgar.tres;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toast.makeText(this,"hola mundito",Toast.LENGTH_LONG).show();
+    }
+
+}
+
+
